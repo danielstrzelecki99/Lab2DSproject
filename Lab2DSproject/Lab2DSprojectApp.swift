40873548153417ccb18f816d2da4dce0ccec5698
Daniel Strzelecki <danielstrzelecki99@wp.pl>
2024-04-27 14:45:22 +0200

    Initial Commit

---
+++ Lab2DSproject/Lab2DSprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2DSprojectApp.swift
+//  Lab2DSproject
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Lab2DSprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
